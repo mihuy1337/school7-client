b9162a56485c055784ea0dfe2051d5e2c4466193
mihuy1337 <kono.mih2020@gmail.com>
2025-03-06 21:45:48 +0300

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
+import WebApp from "@twa-dev/sdk"
 import { ReactNode } from "react"
 
 interface Props {
   children: ReactNode
 }
 
-export function Header({children}: Props) {
+export function Header({ children }: Props) {
+  const platform = WebApp.platform === "android" || WebApp.platform === "android_x" || WebApp.platform === "ios";
+
+  if (!platform) return null;
+
   return (
     <header className="flex justify-center items-center min-h-safe-tg-top">
-      <div className="text-center font-medium rounded-[8px] bg-black-secondary text-accent px-3 py-0.5 ">
+      <div className="text-center font-medium rounded-[8px] bg-black-secondary text-accent px-3 py-0.5">
         {children}
       </div>
     </header>
-  )
+  );
 }
