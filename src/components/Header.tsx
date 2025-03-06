e1d55cb0235177dff6a9d05ffb7eb9a4305b5672
mihuy1337 <kono.mih2020@gmail.com>
2025-03-06 20:58:58 +0300

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ interface Props {
 export function Header({children}: Props) {
   return (
     <header className="flex justify-center items-center min-h-safe-tg-top">
-      <div className="text-center w-full">
-        <div>
+      <div className="text-center font-medium rounded-[7px] bg-accent text-black-main px-2 pt-0.5 ">
+        <div className="">
           {children}
         </div>
       </div>
