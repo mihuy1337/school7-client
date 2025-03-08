2c7ac974ce46d4f49543efe956d7474353fae2c3
mihuy1337 <kono.mih2020@gmail.com>
2025-03-08 22:10:15 +0300

    first commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import { Header } from "../components/Header"
 
 export function MainPage() {
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <Header>Главная</Header>
       <div>Жди докс</div>
     </div>
