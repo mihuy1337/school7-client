131ceb327569542c515f8d0dbfc044b73acd68e7
mihuy1337 <kono.mih2020@gmail.com>
2025-03-09 22:25:03 +0300

    first commit

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -8,7 +8,7 @@ export function Loading() {
       <Header>Загрузка</Header>
       <div className="flex flex-col space-y-8 h-screen justify-center items-center bg-black-main">
         <img className="h-auto max-w-full" src='/pic4.svg'/>
-        <h1 className="font-rubik text-2xl font-semibold text-center">
+        <h1 className="h2 text-center">
           <span className="text-accent">Подожди</span>! Идет загрузка...
         </h1>
         <MyMainButton progress={true} text="Идет загрузка..." disabled={true} color={Colors.black.secondary} textColor={Colors.accent}/>
