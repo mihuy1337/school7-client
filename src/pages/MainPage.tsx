4a3f8ba76dc1ab4eb178b5e158b120cefa66fbcf
mihuy1337 <kono.mih2020@gmail.com>
2025-03-11 21:13:22 +0300

    first commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,15 @@
-import WebApp from "@twa-dev/sdk"
 import { Header } from "../components/Header"
 import { MyMainButton } from "../components/MyMainButton"
-import { Popup } from "../components/Popup"
 import { MySecondaryButton } from "../components/MySecondaryButton"
-import { useShortcut } from "../hooks/useShorcut"
 import { Page404 } from "../components/404"
 import { useNavigate } from "react-router"
 
 export function MainPage() {
   const navigate = useNavigate()
-  const { isShortcut } = useShortcut()
   return (
     <>
       <Header>Главная</Header>
       <Page404 isPage={false}/>
-      {!isShortcut && (
-        <Popup>Для быстрого доступа <span className="text-accent">установи иконку</span> на рабочий стол.</Popup>
-      )}
       <MyMainButton 
         text="Дневник" 
         onClick={() => navigate("/404")}
