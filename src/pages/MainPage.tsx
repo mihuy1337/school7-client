11f4c0877babb21cbc220800f77aede7cc002ff1
mihuy1337 <kono.mih2020@gmail.com>
2025-03-11 21:10:31 +0300

    first commit

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,32 +6,25 @@ import { MySecondaryButton } from "../components/MySecondaryButton"
 import { useShortcut } from "../hooks/useShorcut"
 import { Page404 } from "../components/404"
 import { useNavigate } from "react-router"
-import { saveToStorage } from "../services/storage.service"
-import { Loading } from "../components/Loading"
 
 export function MainPage() {
-  const navigate = useNavigate();
-  const { isShortcut } = useShortcut();
-
-  if (isShortcut === null) return <Loading/>;
-
+  const navigate = useNavigate()
+  const { isShortcut } = useShortcut()
   return (
     <>
       <Header>Главная</Header>
-      <Page404 isPage={false} />
+      <Page404 isPage={false}/>
       {!isShortcut && (
-        <Popup>
-          Для быстрого доступа <span className="text-accent">установи иконку</span> на рабочий стол.
-        </Popup>
+        <Popup>Для быстрого доступа <span className="text-accent">установи иконку</span> на рабочий стол.</Popup>
       )}
-      <MyMainButton
-        text={!isShortcut ? "Да, давай" : "Дневник"}
-        onClick={!isShortcut ? () => WebApp.addToHomeScreen() : () => navigate("/404")}
+      <MyMainButton 
+        text="Дневник" 
+        onClick={() => navigate("/404")}
       />
-      <MySecondaryButton
-        text={!isShortcut ? "Нет, не хочу" : "Д/З"}
-        onClick={!isShortcut ? () => saveToStorage("isMissed", "true") : () => navigate("/404")}
+      <MySecondaryButton 
+        text="Д/З"
+        onClick={() => navigate("/404")}
       />
     </>
-  );
+  )
 }
