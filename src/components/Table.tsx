2de6d2828c3f68a2b3d9bdf3959fdc1b24646484
mihuy1337 <kono.mih2020@gmail.com>
2025-03-16 21:52:42 +0300

    first commit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,7 +14,7 @@ export function Table({data, H1}: TableProps) {
   return (
     <div className="space-y-2">
       <h1 className="font-medium text-xl text-black-hint">{H1}</h1>
-      <div className="px-4 bg-black-secondary rounded-lg w-full mobile-large:text-[14px]">
+      <div className="px-4 bg-black-secondary rounded-lg w-full">
         {data.map((item, index) => (
           <div className="inline-flex justify-between w-full font-medium py-4 border-b-2 border-b-black-hint last:border-b-0" key={index}>
             <div className="ml-1">{`${item.number}. ${item.name}`}</div>
