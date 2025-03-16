4504cf45871d417052dc3b86e2b6563cb4c6ef47
mihuy1337 <kono.mih2020@gmail.com>
2025-03-16 21:16:29 +0300

    first commit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,12 +14,12 @@ export function Table({data, H1}: TableProps) {
   return (
     <div>
       <h1>{H1}</h1>
-      <table>
-        <tbody>
+      <table className="bg-black-secondary rounded-lg p-4">
+        <tbody className="font-medium">
           {data.map((item, index) => (
-            <tr key={index}>
+            <tr className="pb-4" key={index}>
               <td>{`${item.number}. ${item.name}`}</td>
-              <td>{item.desc}</td>
+              <td className="text-right">{item.desc}</td>
             </tr>
           ))}
         </tbody>
