d7f73822f516a7c3bad7c98eac3b6b07bf72ee95
mihuy1337 <kono.mih2020@gmail.com>
2025-03-16 22:48:05 +0300

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export const Header = ({ children }: Props) => {
   return (
     <>
       <div className="h-safe-device-top absolute bg-3-main"></div>
-      <header className="sticky top-safe-device-top w-full flex justify-center items-center min-h-safe-tg-top z-40 bg-transparent"
+      <header className="sticky bg-2-main top-safe-device-top w-full flex justify-center items-center min-h-safe-tg-top z-40"
         style={{
           marginTop: "var(--spacing-header-padding)",
         }}>
