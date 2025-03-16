a63075ded1bd0752f327de7a098ec57dcaed7ff2
mihuy1337 <kono.mih2020@gmail.com>
2025-03-16 21:35:55 +0300

    first commit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -16,7 +16,7 @@ export function Table({data, H1}: TableProps) {
       <h1>{H1}</h1>
       <div className="p-4 bg-black-secondary rounded-lg w-full">
         {data.map((item, index) => (
-          <div className="flex justify-between w-full font-medium" key={index}>
+          <div className="flex justify-between w-full font-medium py-4 border-b-2 border-b-black-hint" key={index}>
             <div>{`${item.number}. ${item.name}`}</div>
             <div>{`${item.desc}`}</div>
           </div>
