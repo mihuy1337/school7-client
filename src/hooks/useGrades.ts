7aca943bc9041f5518c78f3e908f438123475dc9
mihuy1337 <kono.mih2020@gmail.com>
2025-03-25 21:44:07 +0300

    first commit

--- src/hooks/useGrades.ts
+++ src/hooks/useGrades.ts
@@ -1,20 +1,10 @@
 import { useQuery } from "@tanstack/react-query"
-import { Grade, ReportGrades, Statistics, Subject, SubjectGrades } from "../types/grades.types"
+import { ReportGrades, Statistics, SubjectGrades } from "../types/grades.types"
 import { gradesService } from "../services/grades.service"
 import dayjs from "dayjs";
 
-interface GradeWithFormattedDate extends Grade {
-  subject: Subject;
-  createdAt: string;
-}
-
-export interface GroupedGrades {
-  subject: Subject;
-  grades: GradeWithFormattedDate[];
-}
-
 export function useGrades() {
-  const today = dayjs().format('DD/MM')
+  const today = dayjs().format('DD/MM');
   const { data, isLoading, isSuccess } = useQuery<ReportGrades>({
     queryKey: ['grades'],
     queryFn: () => gradesService.getGrades(),
@@ -25,32 +15,27 @@ export function useGrades() {
     return {
       isLoading,
       isSuccess,
-      latestGroupedGrades: [],
+      sortedGrades: [],
       statistics: null,
       newGrades: [],
     };
   }
 
   const statistics: Statistics = data.statistics;
-  const gradesSubjects: SubjectGrades[] = data.subjects;
-
-  for (let i = 0; i < gradesSubjects.length; i++) {
-    let subject = gradesSubjects[i] 
-    subject.grades.sort((a, b) => dayjs(a.createdAt).diff(dayjs(b.createdAt)))
-    subject.grades.map((grade) => {
-      const formattedDay = dayjs(grade.createdAt).format('DD/MM');
-      grade.createdAt = formattedDay
-    })
-  }
-
-  let copyGradesSubjects: SubjectGrades[] = JSON.parse(JSON.stringify(gradesSubjects))
-
-  copyGradesSubjects = copyGradesSubjects
-  .map((subject) => ({
+  const gradesSubjects: SubjectGrades[] = data.subjects.map((subject) => ({
     ...subject,
-    grades: subject.grades.filter((grade) => grade.createdAt === today),
-  }))
-  .filter((subject) => subject.grades.length > 0);
+    grades: [...subject.grades].sort((a, b) => dayjs(a.createdAt).diff(dayjs(b.createdAt))).map((grade) => ({
+      ...grade,
+      createdAt: dayjs(grade.createdAt).format('DD/MM'),
+    })),
+  }));
+
+  const copyGradesSubjects: SubjectGrades[] = gradesSubjects
+    .map((subject) => ({
+      ...subject,
+      grades: subject.grades.filter((grade) => grade.createdAt === today),
+    }))
+    .filter((subject) => subject.grades.length > 0);
 
   return {
     isLoading,
